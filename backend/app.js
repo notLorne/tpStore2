d77d403877c86a6a15ba6822cd36ab7a224055f5
notLorne <113377659+notLorne@users.noreply.github.com>
2023-06-21 18:12:43 -0400

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -1,140 +1,87 @@
+//CONSTANTS
+
 const express = require('express');
-const mysql = require('mysql2'); // Use mysql2 instead of mysql cause authentification mecanism problem
-
-
+const mysql = require('mysql');
 const app = express();
 const port = 3000;
-  
-app.listen(port, () => {
-  console.log(`Serveur express sur le port ${port}`)
-})
-
-
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
-  password: 'MySQL181',
-  database: 'labo2'
+  port: 3306,
+  user: 'tp_account_1',
+  password: 'test',
+  database: 'db_store'
 });
 
-app.get('/mysql', (req, res) => {
+//TABLES DEFINITIONS
 
-  console.log("Route /mysql");
-  connection.connect();
+const createClientsTable = `
+  CREATE TABLE clients (
+    id_client INT AUTO_INCREMENT,
+    nom VARCHAR(255),
+    prenom VARCHAR(255),
+    courriel VARCHAR(255),
+    password VARCHAR(255),
+    PRIMARY KEY (id_client)
+  );`;
 
-  // Création des tables
-  var createClientTable = `
-    CREATE TABLE Client (
-      id_client INT AUTO_INCREMENT,
-      nom VARCHAR(255),
-      prenom VARCHAR(255),
-      courriel VARCHAR(255),
-      password VARCHAR(255),
-      PRIMARY KEY (id_client)
-    );`;
+const createProduitTable = `
+  CREATE TABLE Produit (
+    id_produit INT AUTO_INCREMENT,
+    nom VARCHAR(255),
+    categorie VARCHAR(255),
+    prix DECIMAL(10,2),
+    materiel VARCHAR(255),
+    gemstone VARCHAR(255),
+    carat DECIMAL(4,2),
+    image_url VARCHAR(1024),
+    PRIMARY KEY (id_produit)
+  );`;
 
-  connection.query(createClientTable, function(err, result) {
-    if (err) throw err;
-    console.log("TABLE Client créée");
-  });
+var createCommandeTable = `
+  CREATE TABLE Commande (
+    id_commande INT AUTO_INCREMENT,
+    quantite INT,
+    PRIMARY KEY (id_commande),
+    FOREIGN KEY (id_client) REFERENCES Client(id_client),
+    FOREIGN KEY (id_produit) REFERENCES Produit(id_produit)
+  );`;
 
-  var createProduitTable = `
-    CREATE TABLE Produit (
-      id_produit INT AUTO_INCREMENT,
-      nom VARCHAR(255),
-      categorie VARCHAR(255),
-      prix DECIMAL(10,2),
-      materiel VARCHAR(255),
-      gemstone VARCHAR(255),
-      carat DECIMAL(4,2),
-      image_url VARCHAR(1024),
-      PRIMARY KEY (id_produit)
-    );`;
+//VARIABLE
 
-  connection.query(createProduitTable, function(err, result) {
-    if (err) throw err;
-    console.log("TABLE Produit créée");
-  });
+//METHODS
 
-
-  // Fonctionne pas yé tard à regarder
-  // var createCommandeTable = `
-  //   CREATE TABLE Commande (
-  //     id_commande INT AUTO_INCREMENT,
-  //     quantite INT,
-  //     PRIMARY KEY (id_commande),
-  //     FOREIGN KEY (id_client) REFERENCES Client(id_client),
-  //     FOREIGN KEY (id_produit) REFERENCES Produit(id_produit)
-  //   );`;
-
-  // connection.query(createCommandeTable, function(err, result) {
-  // if (err) throw err;
-  // console.log("TABLE Commande créée");
-  // });
-  
-  connection.end(); 
-
+app.listen(port, (res, req) => {
+  console.log(`Serveur express sur le port ${port}`);
+  console.log(req);
 });
 
+function createDBTable() {
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database: ', err);
+      return;
+    }
+    console.log('Connected to the database!');
+    // Create Tables
 
-// ********** POT DE CODE / COPY_PASTE **********
+    connection.query(createClientsTable, function(err, result) {
+      if (err) throw err;
+      console.log("TABLE Client créée");
+    });
 
-// *** Peut être utile mais utiliser si necessaire ***
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.urlencoded({ extended: true }));
+    connection.query(createProduitTable, function(err, result) {
+      if (err) throw err;
+      console.log("TABLE Produit créée");
+      });
 
-// const cors = require('cors')
-// app.use(cors())
+    connection.end((err) => {
+      if (err) {
+        console.error('Error closing the database connection: ', err);
+        return;
+      }
+      console.log('Connection closed.');
+    });
+  });
+}
 
-// **********************************************
-// Exemple du prof - relation avec index.ejs
-
-// app.set("view engine", "ejs");
- 
-//   // Initialisation du tableau avec les valenodemonrs
-//   var langues = ["français", "anglais", "espanol"];
-
-//   app.get('/ejs', (req, res) => {
-//     res.render("index", {myLanguages: langues})
-//   })
-
-// //Recevoir la réponse du formulaire a partir du ejs
-//   app.post('/ejs', (req, res) => {
-//     // Ajouter une langue
-//     langues.push(req.body.newLanguage)
-//     res.redirect("/ejs")
-//   })
-
-// **** Code isa qui servait pas pour l'instant ****
-// connection.query('SELECT * FROM Departement', function (error, results) {
-//   if (error) throw error;
-//   console.log('Les départements sont: ', results);
-// });
-
-// var createCartTable = `
-//   CREATE TABLE panier (
-//     id_panier INT AUTO_INCREMENT,
-//     id_client INT,
-//     id_produit INT,
-//     quantite INT,
-//     PRIMARY KEY (id_panier),
-//     FOREIGN KEY (id_client) REFERENCES client(id),
-//     FOREIGN KEY (id_produit) REFERENCES produits(id_produit)
-//   );`;
-
-// connection.query(createCartTable, function(err, result) {
-//   if (err) throw err;
-//   console.log("Table panier créée");
-// });
-
-// var createPaypalTable = `
-//   CREATE TABLE paypal (
-//     id_client INT,
-//     info_paiement VARCHAR(255),
-//     FOREIGN KEY (id_client) REFERENCES client(id_client)
-//   );`;
-
-// connection.query(createPaypalTable, function(err, result) {
-//   if (err) throw err;
-//   console.log("Table paypal créée");
-// });
+createDBTable();
